b78b844046f7881a8d9d961724435ba817e26399
Youyuan Wu <youyuanwu@outlook.com>
2024-07-23 17:43:16 -0700

    add tests fro com refcounting

--- crates/libs/core/src/strings.rs
+++ crates/libs/core/src/strings.rs
@@ -113,3 +113,139 @@ mod test {
         assert_eq!(haddr, haddr2);
     }
 }
+
+#[cfg(test)]
+mod com_test {
+    use std::{
+        cell::Cell,
+        sync::{Arc, Mutex},
+        time::Duration,
+    };
+
+    use mssf_com::FabricCommon::{IFabricStringResult, IFabricStringResult_Impl};
+    use windows_core::{implement, AsImpl, HSTRING, PCWSTR};
+
+    #[implement(IFabricStringResult)]
+    pub struct CountingString {
+        data: HSTRING,
+        pub counter: Counter,
+    }
+
+    impl CountingString {
+        pub fn new(data: HSTRING, counter: Counter) -> Self {
+            Self { data, counter }
+        }
+
+        pub fn my_action(self, h: tokio::runtime::Handle) -> IFabricStringResult {
+            assert!(self.counter.get_count() >= 1);
+            let com1: IFabricStringResult = self.into();
+            let com2 = com1.clone();
+            h.spawn(async move {
+                tokio::time::sleep(Duration::from_millis(10)).await;
+                let s: &CountingString = unsafe { com2.as_impl() };
+                assert_eq!(s.counter.get_count(), 2);
+            });
+            com1
+        }
+    }
+
+    impl IFabricStringResult_Impl for CountingString {
+        fn get_String(&self) -> PCWSTR {
+            PCWSTR(self.data.as_ptr())
+        }
+    }
+
+    // The count value is the number of copies it currently has.
+    pub struct Counter {
+        count: Arc<Mutex<Cell<u32>>>,
+    }
+
+    impl Default for Counter {
+        // New counter has value 1.
+        fn default() -> Self {
+            Self {
+                count: Arc::new(Mutex::new(Cell::new(1))),
+            }
+        }
+    }
+
+    impl Counter {
+        pub fn get_count(&self) -> u32 {
+            self.count.lock().unwrap().get()
+        }
+    }
+
+    // increment 1 when cloned
+    impl Clone for Counter {
+        fn clone(&self) -> Self {
+            let mut lk = self.count.lock().unwrap();
+            *lk.get_mut() += 1;
+            Self {
+                count: self.count.clone(),
+            }
+        }
+    }
+
+    // decrement 1 when dropped
+    impl Drop for Counter {
+        fn drop(&mut self) {
+            let mut lk = self.count.lock().unwrap();
+            *lk.get_mut() -= 1;
+        }
+    }
+
+    #[test]
+    fn counter_test() {
+        let c = Counter::default();
+        assert_eq!(c.get_count(), 1);
+        let c2 = c.clone();
+        assert_eq!(c.get_count(), 2);
+        assert_eq!(c2.get_count(), 2);
+        std::mem::drop(c2);
+        assert_eq!(c.get_count(), 1);
+    }
+
+    #[test]
+    fn com_ref_count_test() {
+        let c = Counter::default();
+        let my_str = CountingString::new(HSTRING::from("mydata"), c.clone());
+        assert_eq!(c.get_count(), 2);
+        // convert com from impl should not decrement ref.
+        let com: IFabricStringResult = my_str.into();
+        assert_eq!(c.get_count(), 2);
+        // clone com should not change counter.
+        let com2 = com.clone();
+        assert_eq!(c.get_count(), 2);
+        // drop com objects
+        std::mem::drop(com2);
+        std::mem::drop(com);
+        assert_eq!(c.get_count(), 1);
+    }
+
+    #[tokio::test]
+    async fn com_ref_count_multi_thread_test() {
+        let h = tokio::runtime::Handle::current();
+        let c = Counter::default();
+        let my_str = CountingString::new(HSTRING::from("mydata"), c.clone());
+        // let com1 : IFabricStringResult = my_str.into();
+        // let com2 = com1.clone();
+        // let h = std::thread::spawn(move||{
+        //     std::thread::sleep(Duration::from_millis(10));
+        //     let s: &CountingString = unsafe { com2.as_impl() };
+        //     assert_eq!(s.counter.get_count(),2);
+        //     com2
+        // });
+        // std::mem::drop(com1);
+        // assert_eq!(c.get_count(), 2);
+        // let com22 = h.join().unwrap();
+        // assert_eq!(c.get_count(), 2);
+        // std::mem::drop(com22);
+        // assert_eq!(c.get_count(), 1);
+        let com = my_str.my_action(h);
+        assert_eq!(c.get_count(), 2);
+        std::mem::drop(com);
+        assert_eq!(c.get_count(), 2);
+        tokio::time::sleep(Duration::from_secs(1)).await;
+        assert_eq!(c.get_count(), 1);
+    }
+}
